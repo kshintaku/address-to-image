d9327c4eae365307a15de71d163667915509d07c
Keith <k.shintaku@gmail.com>
2019-02-21 14:56:59 -0800

    initial test

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 var request = require('request');
+var jar = request.jar();
+var cookie = request.cookie("name=Jon");
 
-console.log("nothing here")
+jar.setCookie(cookie);
+
+var propertiesObject = {
+    location:'2645%20west%20231st',
+    start:'0',
+    count:'10',
+    v:'2',
+    market:'socal',
+    ai:'1',
+    iss:'false',
+    ooa:'true',
+    mrs:'false',
+    region_id:'37626',
+    region_type:'2'
+};
+
+request({url:'http://www.redfin.com/stingray/do/location-autocomplete?', qs:propertiesObject, jar:jar}, function(err, response, body) {
+    if(err) { console.log(err); return; }
+    console.log("Get response: " + response.statusCode);
+    // console.log("Get response: " + response.statusMessage);
+    console.log("Get body: " + body);
+});
