7d753e99695f19759782ca72ef810177f58bfb9e
Keith <k.shintaku@gmail.com>
2019-02-22 13:19:02 -0800

    got request to work for retrieving address url

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 var request = require('request');
-var jar = request.jar();
-var cookie = request.cookie("name=Jon");
-
-jar.setCookie(cookie);
 
 var propertiesObject = {
-    location:'2645%20west%20231st',
-    start:'0',
-    count:'10',
-    v:'2',
-    market:'socal',
-    ai:'1',
-    iss:'false',
-    ooa:'true',
-    mrs:'false',
-    region_id:'37626',
-    region_type:'2'
+    location: '2645%20west%20231st',
+    start: '0',
+    count: '10',
+    v: '2',
+    market: 'socal',
+    ai: '1',
+    iss: 'false',
+    ooa: 'true',
+    mrs: 'false',
+    region_id: '37626',
+    region_type: '2'
 };
 
-request({url:'http://www.redfin.com/stingray/do/location-autocomplete?', qs:propertiesObject, jar:jar}, function(err, response, body) {
-    if(err) { console.log(err); return; }
-    console.log("Get response: " + response.statusCode);
-    // console.log("Get response: " + response.statusMessage);
-    console.log("Get body: " + body);
-});
+console.log(addressToURL(propertiesObject.location));
+
+function addressToURL(address) {
+    var redfinURL = 'https://www.redfin.com/stingray/do/location-autocomplete?'
+    var redURL = redfinURL + 'location=' + address;
+    redURL = redURL + '&start=0&count=10&v=2&market=socal&al=1&iss=false&ooa=true&mrs=false&region_id=37626&region_type=2';
+
+    request({ url: redURL }, function (err, response, body) {
+        if (err) { console.log(err); return; }
+        var jsonObject = JSON.parse(body.slice(4));
+        console.log('URL: ' + jsonObject.payload.sections[0].rows[0].url);
+    });
+}
