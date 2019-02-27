3b8d7707841ddc2514058a179d75d45042da26af
Keith <k.shintaku@gmail.com>
2019-02-27 08:42:46 -0800

    working GET for redfin

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var request = require('request');
-var $ = require('cheerio');
+var rp = require('request-promise');
+var express = require('express');
+var cheerio = require('cheerio');
+var app = express();
 
-var propertiesObject = {
+var redfinObject = {
     location: '2645%20west%20231st',
     start: '0',
     count: '10',
@@ -15,29 +18,46 @@ var propertiesObject = {
     region_type: '2'
 };
 
-addressToURL(propertiesObject.location);
+app.get('/propertyPhoto/:address', function (req, res) {
+    const address = req.params.address;
+    console.log('Address received: ' + address);
+    var newUrl = buildRequest(address);
+    makeRedfinRequest(newUrl)
+        .then(redfinURL => getImageUrl(redfinURL))
+        .then(imgUrl => buildResponse(imgUrl))
+        .then(keithResponse => res.status(200).send(keithResponse))
+        .catch(err => res.status(500).send());
+});
 
-function addressToURL(address) {
-    var redImgUrl = 'https://www.redfin.com';
+app.listen(3000, function() {
+    console.log("Started on PORT 3000");
+});
+
+function buildRequest(address) {
     var redfinURL = 'https://www.redfin.com/stingray/do/location-autocomplete?'
     var redURL = redfinURL + 'location=' + address;
     redURL = redURL + '&start=0&count=10&v=2&market=socal&al=1&iss=false&ooa=true&mrs=false&region_id=37626&region_type=2';
-
-    request({ url: redURL }, function (err, response, body) {
-        if (err) { console.log(err); return; }
-        var jsonObject = JSON.parse(body.slice(4));
-        redImgUrl = redImgUrl + jsonObject.payload.sections[0].rows[0].url;
-        urlToImgLink(redImgUrl);
-        console.log('URL: ' + redImgUrl);
-    });
+    return redURL;
 }
 
-function urlToImgLink(url) {
-    // This will grab the image url from the individual property page
-    // Sample redfin image url: 'https://www.redfin.com/PA/Harrisburg/4242-Locust-Ln-17109/home/123764313'
-    request({ url: url }, function (err, response, body) {
-        if (err) { console.log(err); return; }
-        // console.log($('.img-card', body).length);
-        console.log($('.img-card', body).attr().src);
-    });
+async function makeRedfinRequest(redfinRequest) {
+    var redImgUrl = 'https://www.redfin.com';
+
+    const body = await rp(redfinRequest);
+    var jsonObject = JSON.parse(body.slice(4));
+    redImgUrl = redImgUrl + jsonObject.payload.sections[0].rows[0].url;
+    return redImgUrl;
+}
+
+async function getImageUrl(redImgUrl) {
+    const body = await rp(redImgUrl);
+    var imgUrl = cheerio('.img-card', body).attr().src;
+    return imgUrl;
+}
+
+function buildResponse(imgUrl) {
+    var keithResponse = {
+        url: imgUrl
+    };
+    return keithResponse;
 }
