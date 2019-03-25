b7659bb3b8d50100f6282c5307fd2477c4f4e945
Keith <k.shintaku@gmail.com>
2019-03-25 13:31:23 -0700

    zillow testing and working initial working Realtor setup

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var request = require('request');
+request = request.defaults({ jar: true });
 var express = require('express');
 var cheerio = require('cheerio');
 var app = express();
@@ -17,6 +18,9 @@ var redfinObject = {
     region_type: '2'
 };
 
+var zillowAddress = '2248+w+230th';
+var realAddress = '4242%2520Locust%2520Ave%252C%2520Long%2520Beach%252C%2520CA%252C%252090807&area_types=address&area_types=neighborhood&area_types=city&area_types=county&area_types=postal_code&area_types=street'
+
 app.get('/propertyPhoto/:address', function (req, res) {
     const address = req.params.address;
     console.log('Address received: ' + address);
@@ -31,6 +35,13 @@ app.get('/propertyPhoto/:address', function (req, res) {
 app.listen(3000, function () {
     console.log("Started on PORT 3000");
 });
+
+// TODO: Better analyze Zillow
+// makeZillowRequest(zillowAddress);
+
+// makeRealtorRequest(realAddress)
+//     .then(realAddUrl => getRealImageUrl(realAddUrl)
+//     .then(imgUrl => console.log(imgUrl)));
 
 function buildRequest(address) {
     var redfinURL = 'https://www.redfin.com/stingray/do/location-autocomplete?'
@@ -74,4 +85,72 @@ function buildResponse(imgUrl) {
         url: imgUrl
     };
     return keithResponse;
+}
+
+function makeZillowRequest(address) {
+    // var zilUrl = 'https://www.zillow.com/search/RealEstateSearch.htm?citystatezip=';
+    var zilUrl = 'https://www.zillow.com/homes/';
+    var zilUrl = zilUrl + address + '_rb/';
+    console.log(zilUrl);
+
+    var options = {
+        method: 'GET',
+        url: 'https://www.zillow.com/homes/2248+w+230th_rb/',
+        headers:
+        {
+            cookie: 'zgsession=1|378a942f-2184-4563-94f1-814ac45578e5; zguid=24|%24d84afb5a-900d-4e20-8a67-935ff842b2d5; abtest=3|DOjqyTP_AMMxPSaQ8w; pxvid=ad344f60-3abc-11e9-93d0-9b44f4fa3e50; _pxvid=ad344f60-3abc-11e9-93d0-9b44f4fa3e50; ajs_user_id=null; ajs_group_id=null; ajs_anonymous_id=%22d84afb5a-900d-4e20-8a67-935ff842b2d5%22; G_ENABLED_IDPS=google; JSESSIONID=54CEF49F78BD1A30E15A31B30F3BABF2; _pxff_tm=1; search=6|1554568957213%7Crect%3D33.830052%252C-118.310137%252C33.80224%252C-118.333097%26zm%3D14%26disp%3Dmap%26mdm%3Dauto%26p%3D1%26z%3D1%26fs%3D1%26fr%3D0%26mmm%3D1%26rs%3D0%26ah%3D0%26singlestory%3D0%09%01%0996159%09%09%092%090%09US_%09; AWSALB=P3XeChNhedsH2o08PcoF0Sb2xR7tnrH0IElgj6sK7hDtAKg62qBb+OVus6VRSu35fAdY3s8CuBC4vut2pISZ5QYgDUC9ff7WAWhrISnyP9+4ISYvdtAH72C9oSkZ; _px3=eee020384ec3a37a1f482f901f924db45484e5383becd0fa78c374f4f86704a8:Y1BjGgQu9aEFys8BGGkgr+C1KeegWTYf+7EPyoaBh70D5gZeVy33v7r5Pwy4q858UOiQriA9sUZzjZsQ0NZ2zw==:1000:wCG5HEOWvURDapg8MJHmcDr125bs4ySyU63HI7xdIUJTNCoCZ2x19aT3S0+VobLVUY1qYVNKac3+qJWRDYkm8dFc7SobS/3u7Wn9avXuw/lVbnTQY6/YPU0NmvjuRVpQ8GS/LvdmOnZkf4HgZmTV186nTaRgjntWqHITnis1NeE=',
+            // 'Postman-Token': 'dd9659d2-8d8e-44b8-8b21-c2a47d9ab4fe',
+            // 'accept-language': 'en-US,en;q=0.9',
+            // 'accept-encoding': 'gzip, deflate, br',
+            accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
+            'upgrade-insecure-requests': '1',
+            'cache-control': 'max-age=0,no-cache',
+            authority: 'www.zillow.com'
+        }
+    };
+
+
+    return new Promise(resolve => {
+        request(options, function (err, response, body) {
+            // request({ url: zilUrl }, function (err, response, body) {
+            if (err) { console.log(err); return; }
+            console.log(body);
+            resolve(body);
+        })
+    }).then(body => {
+        var imgUrl = cheerio('.photo-tile-image', body).attr().src;
+        return imgUrl;
+    })
+}
+
+function makeRealtorRequest(address) {
+    var realUrl = 'https://www.realtor.com/api/v1/geo-landing/parser/suggest/?input=' + address;
+    var realAddUrl = 'https://www.realtor.com/realestateandhomes-detail/M';
+
+    return new Promise(resolve => {
+        request({ url: realUrl }, function (err, response, body) {
+            if (err) { console.log(err); return; }
+            resolve(body);
+        })
+    }).then(body => {
+        var jsonObject = JSON.parse(body);
+        try { realAddUrl = realAddUrl + jsonObject.result[0].mpr_id; }
+        catch (err) { throw 'address not found'; }
+        console.log(realAddUrl);
+        return realAddUrl;
+    })
+}
+
+function getRealImageUrl(realAddUrl) {
+    return new Promise(resolve => {
+        request({ url: realAddUrl }, function (err, response, body) {
+            if (err) { console.log(err); return; }
+            resolve(body);
+        })
+    }).then(body => {
+        // console.log(body);
+        var imgUrl = cheerio('.modal-heroimg', body).children()[0].attribs.src;
+        return imgUrl;
+    })
 }
